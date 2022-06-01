301b93c1952fbaf358b46f80c68f6d4ea7250ff3
Paul Wellner Bou <paul@wellnerbou.de>
2022-06-01 08:40:58 +0200

    Add some keywords

--- app.js
+++ app.js
@@ -18,44 +18,47 @@ const app = new App({
     console.log('Fetching users...')
     try {
         const result = await app.client.users.list();
-        console.debug(result.members)
-
         users = result.members.map(user => ({
             id: user.id,
             username: user.profile.real_name
         }));
         console.debug(users)
-      }
-      catch (error) {
+    }
+    catch (error) {
         console.error(error);
-      }
+    }
     console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
 })();
 
 // This will match any message that contains 👋
 app.message(':wave:', async ({ message, say }) => {
     console.debug("Received :wave:")
-    await say(`Hello, <@${message.user}>`);
+    let ts = message.thread_ts || message.ts
+    await say({ text: `Hello, <@${message.user}>`, thread_ts: ts });
 });
 
-app.event('app_mention', async ({ event, message, context, client, say }) => {
+app.event('app_mention', async ({ event, context, client, say }) => {
     console.debug(event)
     let ts = event.thread_ts || event.ts
-    try {
-        let conversations = await app.client.conversations.replies({
-            token: process.env.SLACK_BOT_TOKEN,
-            channel: event.channel,
-            ts: ts
-        })
-        let originalMessage = conversations.messages.filter(message => message.ts == ts)[0]
-        console.debug(originalMessage)
-        console.debug(originalMessage.text)
-        response = await createTrelloCard(originalMessage)
-        console.debug(response)
-        await say({ text: `Thanks, <@${event.user}>, I created this card: ${response.url}`, thread_ts: ts })
-    } catch (error) {
-        console.error(error);
-        await say({ text: `<@${event.user}>, there was an error: \`${error}\``, thread_ts: ts })
+    if (event.text.includes("backup") || event.text.includes("back up") || event.text.includes("trello") || event.text.includes("card") || event.text.includes("save")) {
+        try {
+            let conversations = await app.client.conversations.replies({
+                token: process.env.SLACK_BOT_TOKEN,
+                channel: event.channel,
+                ts: ts
+            })
+            let originalMessage = conversations.messages.filter(message => message.ts == ts)[0]
+            console.debug(originalMessage)
+            console.debug(originalMessage.text)
+            response = await createTrelloCard(originalMessage)
+            console.debug(response)
+            await say({ text: `Thanks, <@${event.user}>, I created this card: ${response.url}`, thread_ts: ts })
+        } catch (error) {
+            console.error(error);
+            await say({ text: `<@${event.user}>, there was an error: \`${error}\``, thread_ts: ts })
+        }
+    } else {
+        await say({ text: `Hello, <@${event.user}>`, thread_ts: ts });
     }
 });
 
